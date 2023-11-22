5ee52f66fa8dcc401c2f08dce3ed0fc1611ce0f2
Fynn Westerath <westerathf@gmail.com>
2023-11-22 23:12:15 +0100

    add about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
+import type { StrapiData } from '~/ts/interfaces/strapi/StrapiData';
+import type { About } from '~/ts/interfaces/About';
 
+const aboutText = ref<StrapiData<About>>();
+
+onMounted(async () => {
+    const response = await fetch(`http://localhost:1337/api/about/`);
+    aboutText.value = await response.json() as StrapiData<About>;
+});
 </script>
 
 <template>
     <h1>About</h1>
+
+    <p>
+        {{ aboutText?.data.attributes.description }}
+    </p>
 </template>
 
 <style lang="scss" scoped>
